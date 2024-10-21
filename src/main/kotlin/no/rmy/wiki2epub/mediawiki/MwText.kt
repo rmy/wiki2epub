211e24d7f23450e87e3b0bf761e82fdabbe6912a
Rune Myrland <rune@skalden.com>
2024-10-21 10:08:00 +0200

    test

--- src/main/kotlin/no/rmy/wiki2epub/mediawiki/MwText.kt
+++
@@ -1,8 +0,0 @@
-package no.rmy.wiki2epub.mediawiki
-
-class MwText(override val parent: MwTag, val value: String) : MwTag {
-    override val name: String get() = "text"
-
-    override fun content(): String = value
-}
-
