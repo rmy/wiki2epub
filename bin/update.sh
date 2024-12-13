c29eefab6b3f1cdcf8a379265f322c8b1e9cbb29
Rune Myrland <rune@skalden.com>
2024-12-13 14:07:29 +0100

    pull

--- bin/update.sh
+++ bin/update.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $SCRIPT_DIR/..
 
 rm -rf files
+git pull
 gradle run
 bin/epubcheck docs/download/iliaden.epub && git commit -a -m updated && git push
 
