f44238bdb30d977726570618bd6aecc9f973a09c
Rune Myrland <rune@skalden.com>
2024-12-14 11:23:28 +0100

    updated

--- bin/update.sh
+++ bin/update.sh
@@ -4,8 +4,10 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $SCRIPT_DIR/..
 
 echo "Starting: $(date)"
-rm -rf files
+rm -rf files/epub*
 git pull
-gradle run
+gradle installDist
+./build/install/wiki2epub/bin/wiki2epub
 bin/epubcheck docs/download/iliaden.epub && git commit -a -m updated && git push
 echo "Finished: $(date)"
+
